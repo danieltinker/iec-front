1095f06a8001875d1fb7f063828bec77c3bbcba4
ranf <ran@headon.co.il>
2022-11-03 14:55:28 +0200

    update

--- src/services/myAxiosRequests.js
+++ src/services/myAxiosRequests.js
@@ -19,7 +19,7 @@ export const myApi = (endPoint) => {
 export const myShavitApiV1 = (endPoint) => {
     return axios({
         method: "post",
-        url: `${store.state.serverAdrr}/shavit/${endPoint}`,
+        url: `${store.state.serverAdrr}/shavit-mobile/${endPoint}`,
         timeout: 1000 * 8, // Wait for 8 seconds
         headers: {
             "Content-Type": "application/json"
