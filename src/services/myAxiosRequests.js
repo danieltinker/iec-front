bec8735d1c53a74e97d5a5f6af56f5926b8cda45
daniel <danielb534@gmail.com>
2022-11-14 08:42:58 +0200

    forgot the slash

--- src/services/myAxiosRequests.js
+++ src/services/myAxiosRequests.js
@@ -5,7 +5,7 @@ import store from "../store";
 export const myApi = (endPoint) => {
     return axios({
         method: "get",
-        url: `${store.state.serverAdrr}/shavit-mobile${endPoint}`,
+        url: `${store.state.serverAdrr}/shavit-mobile/${endPoint}`,
         timeout: 1000 * 8, // Wait for 8 seconds
         headers: {
             "Content-Type": "application/json"
@@ -19,7 +19,7 @@ export const myApi = (endPoint) => {
 export const myShavitApiV1 = (endPoint) => {
     return axios({
         method: "post",
-        url: `${store.state.serverAdrr}/shavit-mobile${endPoint}`,
+        url: `${store.state.serverAdrr}/shavit-mobile/${endPoint}`,
         timeout: 1000 * 8, // Wait for 8 seconds
         headers: {
             "Content-Type": "application/json"
