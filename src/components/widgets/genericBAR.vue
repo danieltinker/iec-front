ade29bcefef7497e4a4c6317440e3faa1c27e20f
daniel <danielb534@gmail.com>
2022-11-15 13:30:10 +0200

    vero

--- src/components/widgets/genericBAR.vue
+++ src/components/widgets/genericBAR.vue
@@ -201,6 +201,7 @@ export default {
 
     async created(){
         if(!this.isDrillDown){
+            this.params.static_drill_titles_param_copy = this.params.static_drill_titles_param
             await this.$myApi(this.params.data_url)
                 .then(response => {
                     this.jsonData = response.data
@@ -244,6 +245,7 @@ export default {
                     this.succ_req = false
                 });
                 if(this.succ_req){
+                    this.params.static_drill_titles_param_copy = this.params.static_drill_titles_param
                     await this.$myApi(this.params.drill_down_params.data_url)
                     .then(response => {
                         this.drilldownData = Object.assign(response.data)
